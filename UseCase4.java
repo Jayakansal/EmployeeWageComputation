d0e6f53904315b5681570527ed363dff9718b1b2
Jayakansal <21jayajindal@gmail.com>
2020-11-28 11:38:43 +0530

    Added: Employee_information_Switch

---
+++ UseCase4.java
@@ -0,0 +1,36 @@
+package com.useCase4;
+
+import java.util.Random;
+
+public class WorkingHour {
+
+
+        public static void main(String[] args) {
+                int PartTime=1;
+                int FullTime=2;
+                int EmpRatePerHr=20;
+                double Salary;
+                int absent=0;
+                int partTimeHour=4;
+                int fullTimeHour=8;
+                Random random=new Random();
+                int randomNum=random.nextInt(3);
+                int EmpHur;
+                switch (randomNum) {
+                case 1:
+                        int partTimeWage=(partTimeHour*EmpRatePerHr);
+                        System.out.println("Part Time Employee " +partTimeWage);
+                        break;
+                case 2:
+                        int fullTimeWage=(fullTimeHour*EmpRatePerHr);
+                        System.out.println("Full Time Employee " +fullTimeWage);
+                        break;
+
+                default:
+                        System.out.println("Absent"+ absent);
+                        break;
+                }
+
+
+        }
+        }
