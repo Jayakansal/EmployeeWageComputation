8d4e291f1a28c8ef07c6a37ca429151cfc57dad9
Jayakansal <21jayajindal@gmail.com>
2020-11-28 10:40:19 +0530

    Added:DailyEmployee_Logic

---
+++ UseCase2.java
@@ -0,0 +1,20 @@
+package com.useCase2;
+
+import java.util.Random;
+
+public class SalaryOfEmployee {
+
+        public static void main(String[] args) {
+                Random random=new Random();
+                int randomNum=random.nextInt(2);
+
+if (randomNum==1) {
+System.out.println("Employee is Present");
+int EmpHur=8;
+int ratePerHur=20;
+System.out.println("Salary:(int EmpHur* int ratePerHur)}");
+
+} else {
+System.out.println("Employee is Absent");
+System.out.println("Salary:+0");
+}
